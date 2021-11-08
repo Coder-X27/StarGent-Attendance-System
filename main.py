6acb37241b59ea5cd4c998ea3aa1807aabbec837
Karan <sharma.karan27oct@gmail.com>
2021-11-08 10:54:01 +0530

    Changed the main

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 # The name of the members start from the B7 
 # The presents start from AH7 and the Absents start from the AI7
 import pyttsx3
-# import speech_recognition as sr
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
 engine.setProperty('voice',voices[0].id)
@@ -10,24 +9,6 @@ engine.setProperty('voice',voices[0].id)
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
-
-# def takeCommand():
-#     r=sr.Recognizer()
-#     with sr.Microphone() as source:
-#         print('Listening')
-#         r.pause_threshold=1
-#         audio=r.listen(source)
-    
-#     try:
-#         print("Recongnizing. . .")
-#         query=r.recognize_google(audio,language='en-in')
-#         print(f'User said: {query}\n')
-
-#     except Exception as e:
-#         # print(e)
-#         print('say that again please. . .')
-#         return 'None'
-#     return query
 
 def identify(user):
     pass
