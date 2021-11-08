598afb5a01350c88cda736612e3fb0c4ccff47dc
Karan <sharma.karan27oct@gmail.com>
2021-11-08 14:51:14 +0530

    speak func removed

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # Here in the spread sheet The code of the members start from the A7 to the end 
 # The name of the members start from the B7 
 # The presents start from AH7 and the Absents start from the AI7
-import pyttsx3
-engine=pyttsx3.init('sapi5')
-voices=engine.getProperty('voices')
-engine.setProperty('voice',voices[0].id)
+# import pyttsx3
+# engine=pyttsx3.init('sapi5')
+# voices=engine.getProperty('voices')
+# engine.setProperty('voice',voices[0].id)
 
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
+# def speak(audio):
+#     engine.say(audio)
+#     engine.runAndWait()
 
 def identify(user):
     pass
@@ -23,14 +23,10 @@ def validation():
 
 
 if __name__=="__main__":
-    # print("Welcome to the StarGent Robotics Biomatrix Attendance System \n")
-    speak("Welcome to the StarGent Robotics Community \n")
-    # choice=int(input("What do you want\n1) Mark Present\n2) Register Yourself as a Community Member\n3) Forgot Code\n4) Show total Presents\n"))
-    speak("What do you want\n1) Mark Present\n2) Register Yourself as a Community Member\n3) Forgot Code\n4) Show total Presents\n")
-    choice=int(input())
+    print("Welcome to the StarGent Robotics Biomatrix Attendance System \n")
+    choice=int(input("What do you want\n1) Mark Present\n2) Register Yourself as a Community Member\n3) Forgot Code\n4) Show total Presents\n"))
     if(choice==1 or choice==4):
         # print("Plz Place your Thumb on the scanner to know your identification\n")
-        speak("Plz Place your Thumb on the scanner to know your identification\n")
         isValid=validation()
         if isValid is True:
             user=identify()
@@ -43,5 +39,4 @@ if __name__=="__main__":
         else:
             print("You are not a part of the Community Plz Register Your self or Contact The head")
     elif(choice==2 or choice==3):
-        # print('Welcome To the Stargent Robotics Community Here Our Motto is to Make Bots and TO make the Gyanni 3.0')
-        speak('Hi There ! Here Our Motto is to Make Bots and TO make the Gyanni 3.0')
+        print('Welcome To the Stargent Robotics Community Here Our Motto is to Make Bots and TO make the Gyanni 3.0')
